9a02ff9b7e0bf863785c2bba373c68ea3d6ef83c
kbwedge <kevin.wedge@kellogg.northwestern.edu>
2022-01-25 17:01:29 +0000

    Lab 4 Complete

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,37 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    name: "Kevin",
+    location: "Chicago",
+    status:"Student"
+}
 
+puts profile
+puts profile[:name]
+
+profile = {
+    name: "Kevin",
+    location: {
+        city: "Chicago",
+        state: "IL"
+    },
+    status:"Student",
+    timeline: [
+        {status: "in class", posted: "8:30am"},
+        {status: "eating lunch", posted: "12:00pm"}
+    ]
+}
+
+puts profile[:location]
+puts profile[:location][:city]
+
+puts "Latest status"
+puts "__________________"
+puts profile[:timeline][1][:status]
 # Accessing data from the hash
+
+
+
 
 # More Complex Hashes
